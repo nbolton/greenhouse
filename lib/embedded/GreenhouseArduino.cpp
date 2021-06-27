424ac593a6db6d2e98581fd7679a4b2c51af999c
Nick Bolton <nick@nbolton.com>
2021-06-27 19:56:58 +0100

    Add missing sync for V23

--- lib/embedded/GreenhouseArduino.cpp
+++ lib/embedded/GreenhouseArduino.cpp
@@ -518,7 +518,7 @@ void GreenhouseArduino::HandleSoilMostureWarning(float soilMostureWarning)
 BLYNK_CONNECTED()
 {
   // read all last known values from Blynk server
-  Blynk.syncVirtual(V0, V3, V5, V8, V9, V14, V15, V16, V17, V18, V22);
+  Blynk.syncVirtual(V0, V3, V5, V8, V9, V14, V15, V16, V17, V18, V22, V23);
 }
 
 BLYNK_WRITE(V0)
