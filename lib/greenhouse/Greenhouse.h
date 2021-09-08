95148589e830f268f7c4f391dbee8a7dfccf5f7e
Nick Bolton <nick@nbolton.com>
2021-09-08 17:11:10 +0100

    Remove unused member function #189

--- lib/greenhouse/Greenhouse.h
+++ lib/greenhouse/Greenhouse.h
@@ -116,7 +116,6 @@ public:
 private:
   void UpdateDayWaterHeating(bool airHeatingRequired, bool soilHeatingRequired);
   void UpdateNightWaterHeating(bool airHeatingRequired, bool soilHeatingRequired);
-  std::string GetWeatherUri() const;
   bool IsRaining() const;
 
 private:
