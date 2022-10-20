bfcbf38c8ec08f77cead9156c26a4a96c8a568e2
Nick Bolton <nick@nbolton.com>
2022-10-21 00:23:00 +0100

    Use correct pin macros

--- firmware/greenhouse-node/src/pins.h
+++ firmware/greenhouse-node/src/pins.h
@@ -2,12 +2,12 @@
 
 #include "attiny.h"
 
-#define PIN_TX_EN PA0
-#define PIN_TX PA1
-#define PIN_RX PA2
-#define PIN_ONE_WIRE PA3
-#define PIN_SR_LATCH PA4  // RCLK (12)
-#define PIN_SR_CLOCK PA5  // SRCLK (11)
-#define PIN_SR_DATA PA6   // SER (14)
-#define PIN_SR_OE PA7     // OE (13)
-#define PIN_MOTOR_PWM PB2
+#define PIN_TX_EN PIN_PA0
+#define PIN_TX PIN_PA1
+#define PIN_RX PIN_PA2
+#define PIN_ONE_WIRE PIN_PA3
+#define PIN_SR_LATCH PIN_PA4  // RCLK (12)
+#define PIN_SR_CLOCK PIN_PA5  // SRCLK (11)
+#define PIN_SR_DATA PIN_PA6   // SER (14)
+#define PIN_SR_OE PIN_PA7     // OE (13)
+#define PIN_MOTOR_PWM PIN_PB2
